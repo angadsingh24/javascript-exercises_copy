c87cf060ca8ad2afd2e11aec2f1f8c7a8d19300b
Angad Singh <angadsinghnj@gmail.com>
2024-06-08 15:49:25 -0400

    04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...moreArgs) {
+    const returnArr = [];
+    for(const i of arr) {
+        let remove = false;
+        for(const arg of moreArgs){
+            if(i === arg) remove=true;
+        }
+        if (remove==false) returnArr.push(i);
+    }
+    return returnArr;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+ 
