561302cbf597db459e329b671da06150660381f8
Angad Singh <angadsinghnj@gmail.com>
2024-06-08 15:23:11 -0400

    02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let strRep = "";
+    if (num<0) return 'ERROR';
+    for (let i=0; i<num;i++){
+        strRep+=str;
+    }
 
+    return strRep;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+ 
